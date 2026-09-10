63cc2ea2e88b6121349d7251f231cc83204ca9da
lucio-18 <lrosenthal@pioix.edu.ar>
2026-09-10 08:27:12 -0300

    Update ChatList.js
    Co-Authored-By: lolobeccaria <216725581+butteler32@users.noreply.github.com>
    Co-Authored-By: gbenitez-coder <241324759+gbenitez-coder@users.noreply.github.com>

--- frontend/src/components/ChatList.js
+++ frontend/src/components/ChatList.js
@@ -1,21 +1,19 @@
 import ChatItem from "./ChatItem";
 
 export default function ChatList({ chats, onSelectChat }) {
-    return (
-        <div className="chat-list">
-
-            {chats.length === 0 ? (
-                <p>No tenés chats todavía.</p>
-            ) : (
-                chats.map((chat) => (
-                    <ChatItem
-                        key={chat.id}
-                        chat={chat}
-                        onClick={() => onSelectChat(chat)}
-                    />
-                ))
-            )}
-
-        </div>
-    );
+  return (
+    <div className="chat-list">
+      {chats.length === 0 ? (
+        <p>No tenés chats todavía.</p>
+      ) : (
+        chats.map((chat) => (
+          <ChatItem
+            key={chat.id}
+            chat={chat}
+            onClick={() => onSelectChat(chat)}
+          />
+        ))
+      )}
+    </div>
+  );
 }
